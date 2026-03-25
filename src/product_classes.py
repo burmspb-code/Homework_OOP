8b39f371aa2e5c9ef95d13d830b51b52b3a80491
Mikhail Byrdukovsky <burmspb@gmail.com>
2026-03-25 21:50:56 +0300

    add classes Smartphone + LawnGrass

---
+++ src/product_classes.py
@@ -0,0 +1,52 @@
+from src.models import Product
+
+
+class Smartphone(Product):
+    """Класс товаров Smartphone - наследник класса Product."""
+
+    def __init__(self, name: str, description: str, price: float, quantity: int, efficiency: str, model: str,
+                 memory: str, color: str):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+    def __add__(self, other):
+        """Сложение продуктов одинокового класса."""
+        if type(other) is Smartphone:
+            return self.quantity + other.quantity
+        else:
+            return TypeError
+
+
+
+class LawnGrass(Product):
+    """Класс товаров LawnGrass - наследник класса Product."""
+
+    def __init__(self, name: str, description: str, price: float, quantity: int, country: str, germination_period: str,
+                 color: str):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
+    def __add__(self, other):
+        """Сложение продуктов одинокового класса."""
+        if type(other) is LawnGrass:
+            return self.quantity + other.quantity
+        else:
+            return TypeError
+
+if __name__ == "__main__":
+    smart_1 = Smartphone("Iphone", "15", 200000.0, 10, "Gud", "6G", "1024K", "Black")
+    smart_2 = Smartphone("Samsung Galaxy", "S23", 230000.0, 5, "Gud+", "5G", "1024K", "Blue")
+
+    lawngrass_1 = LawnGrass("Газон 'Городской", "Устойчивая", 450.0, 15, "Россия", "10-14 дней", "Темно-зеленый")
+    lawngrass_2 = LawnGrass("Мятлик луговой", "Плотный покров", 1250.0, 5, "Нидерланды", "21-28 дней", "Изумрудный")
+
+    print(lawngrass_1 + lawngrass_2)
+
+
+
+
