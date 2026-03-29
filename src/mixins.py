4ce9f83fe58b7dd6abeed7b3c18cb29af3b85f6d
Mikhail Byrdukovsky <burmspb@gmail.com>
2026-03-29 22:20:11 +0300

    add MixinLog in mixins.py

--- src/mixins.py
+++ src/mixins.py
@@ -5,7 +5,7 @@ class MixinLog:
 
     def __init__(self, *args, **kwargs):
         super().__init__()
-        print(repr(self))
+        print(repr(self)) # Вывод параметров объекта в консоль при создании
 
     def __repr__(self):
         params_str = [f"{k}={v!r}" for k,v in self.__dict__.items()] # Создаем список строк с параметрами объекта, !r - заключает в кавычки
